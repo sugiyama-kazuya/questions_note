cce1a14ec17f1d096586550af44333e6e41a07e2
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-05-06 16:16:22 +0900

    ヘッダーとフッターをコンポーネントとして切り分けました。
    問題作成画面を作成しました

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="flex mb-4 h-20 absolute inset-x-0 top-0">
+    <div class="w-1/3 bg-blue-300"></div>
+    <div class="w-1/3 bg-blue-300 text-center text-white flex items-center justify-center">
+      <h4 class="m-0">Home</h4>
+    </div>
+    <div class="w-1/3 bg-blue-300"></div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
