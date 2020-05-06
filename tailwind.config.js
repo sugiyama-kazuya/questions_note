66b5cf64cae60b356beb182715ece364542b9f4b
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-05-06 23:48:50 +0900

    cssのclassを追加しました

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    purge: [],
+    theme: {
+        top: {
+            "1": "0",
+            "1/2": "50%"
+        },
+        height: {
+            "50": "75%"
+        },
+        padding: {
+            "9rem": "9rem"
+        },
+        extend: {}
+    },
+    variants: {},
+    plugins: []
+};
