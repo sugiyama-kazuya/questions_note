be41d8825c93ed20e4c91e7f7ee6042c34dfb0e4
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-05-09 22:50:33 +0900

    問題集、問題、解答のテーブルを作成

---
+++ database/migrations/2020_05_09_121907_create_exercise_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExerciseBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercise_books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('problem_id')->unsigned();
+            $table->foreign('problem_id')->references('id')->on('problems');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercise_books');
+    }
+}
