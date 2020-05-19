c8e42776521c0f29a8be1a65864e82a3553b277b
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-05-19 14:22:35 +0900

    不要なcssを削除

--- resources/js/components/CenterModal.vue
+++ resources/js/components/CenterModal.vue
@@ -1,43 +1,34 @@
 <template>
-  <div class="modal-mask relative">
-    <div class="bg-white rounded w-2/3 m-0 m-auto shadow modal-view">
-      <slot></slot>
+    <div class="modal-mask relative">
+        <div class="bg-white rounded w-2/3 m-0 m-auto shadow modal-view">
+            <slot></slot>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "CenterModal"
+    name: "CenterModal"
 };
 </script>
 
-<style>
+<style scoped>
 .modal-mask {
-  position: fixed;
-  z-index: 9999;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
+    position: fixed;
+    z-index: 9999;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
 }
 
 .modal-view {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-.validateError-enter-active,
-.validateError-leave-active {
-  transition: opacity 0.5s;
-}
-
-.validateError-enter,
-.validateError-leave-to {
-  opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
