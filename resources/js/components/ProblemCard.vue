ed1ecda7d723d42f59447db524a3579ec4231258
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-05-24 15:43:38 +0900

    問題カードをクリック後の遷移先を指定

--- resources/js/components/ProblemCard.vue
+++ resources/js/components/ProblemCard.vue
@@ -2,23 +2,33 @@
   <div class="bg-white mx-3 rounded-sm shadow-sm px-2 flex flex-column">
     <div @click="goProblem(cardData.id)" class="flex flex-column">
       <div class="text-right p-2">カテゴリ</div>
-      <div class="px-2 pb-4 pt-2">{{cardData.exerciseBooksName.name}}</div>
+      <div class="px-2 pb-4 pt-2">{{ cardData.exerciseBooksName.name }}</div>
     </div>
     <div class="border-t p-3 flex items-center justify-between text-gray-600">
       <div class="flex items-center w-1/3">
-        <font-awesome-icon icon="user-circle" class="text-2xl mr-2"></font-awesome-icon>
-        {{cardData.user.name}}
+        <router-link
+          tag="div"
+          :to="{
+                        name: 'profile',
+                        params: {
+                            userId: cardData.user.id
+                        }
+                    }"
+        >
+          <font-awesome-icon icon="user-circle" class="text-2xl mr-2"></font-awesome-icon>
+          {{ cardData.user.name }}
+        </router-link>
       </div>
       <div class="flex w-1/3">
         <font-awesome-icon
           @click="isLike(cardData.id)"
           icon="star"
           class="text-2xl mr-2"
-          :class="{ 'is-like' : isLikedBy }"
+          :class="{ 'is-like': isLikedBy }"
         />
-        <span>{{count}}</span>
+        <span>{{ count }}</span>
       </div>
-      <div class="w-1/3 text-right">{{cardData.updated_at}}</div>
+      <div class="w-1/3 text-right">{{ cardData.updated_at }}</div>
     </div>
   </div>
 </template>
@@ -49,13 +59,17 @@ export default {
   },
   methods: {
     goProblem(exerciseBooksId) {
-      this.$router.push({ name: "problem", params: { id: exerciseBooksId } });
+      this.$router.push({
+        name: "problem",
+        params: { id: exerciseBooksId }
+      });
     },
     async isLike(problemId) {
       // 既にいいねしていた場合、していなかった場合
       if (this.isLikedBy) {
+        console.log("true");
         const response = await axios
-          .delete("api/unlike", { data: { id: problemId } })
+          .delete("/api/unlike", { data: { id: problemId } })
           .catch(error => error.response);
 
         if (response.status === INTERNAL_SERVER_ERROR) {
@@ -66,7 +80,7 @@ export default {
         this.isLikedBy = false;
       } else {
         const response = await axios
-          .put("api/like", { id: problemId })
+          .put("/api/like", { id: problemId })
           .catch(error => error.response || error);
 
         if (response.status === INTERNAL_SERVER_ERROR) {
@@ -78,7 +92,7 @@ export default {
       }
     },
     async isLikedByApi(id) {
-      const url = `api/islikedby/${id}`;
+      const url = `/api/islikedby/${id}`;
       const response = await axios
         .get(url)
         .catch(error => error.response || error);
@@ -91,7 +105,7 @@ export default {
       return response.data.isLike;
     },
     async countLikes(id) {
-      const countLikesUrl = `api/countLikes/${id}`;
+      const countLikesUrl = `/api/countLikes/${id}`;
       const response = await axios
         .get(countLikesUrl)
         .catch(error => error.response);
@@ -105,6 +119,7 @@ export default {
     }
   },
   async mounted() {
+    console.log(this.cardData.id);
     const isLike = await this.isLikedByApi(this.cardData.id);
     const countLikes = await this.countLikes(this.cardData.id);
 
