6081ae253b3469fbe38eb7e6328f008bb66a133b
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-05-25 16:07:41 +0900

    問題集一覧画面で使用していた切り替えタブボタンをコンポーネント化

---
+++ resources/js/components/ChangeTabBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="inline-flex w-4/5 st-border h-60">
+    <button
+      @click="$emit('left-click')"
+      class="w-1/2 bg-white text-indigo-500 py-2 px-4 focus:outline-none"
+      :class="{ 'is-tab-active': isLeftActive }"
+    >
+      <slot name="leftBtnText" />
+    </button>
+    <button
+      @click="$emit('right-click')"
+      class="w-1/2 bg-white py-2 px-4 text-indigo-500 focus:outline-none"
+      :class="{ 'is-tab-active': isRightActive }"
+    >
+      <slot name="rightBtnText" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isLeftActive: {
+      type: Boolean
+    },
+    isRightActive: {
+      type: Boolean
+    }
+  }
+};
+</script>
+
+<style scoped>
+.is-tab-active {
+  background-color: #667eea !important;
+  color: #fff !important;
+}
+</style>
