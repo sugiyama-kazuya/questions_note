8c6735f9dd04036fd00eebb09a013accba8a7274
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-06-14 10:44:08 +0900

    モジュールの削除と追加

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -3,9 +3,8 @@ import Vuex from "vuex";
 
 import auth from "./auth";
 import error from "./error";
-import createForm from "./createForm";
+import problem from "./problem";
 import listProblem from "./listProblem";
-import playProblem from "./playProblem";
 import flashMessage from "./flashMessage";
 
 Vue.use(Vuex);
@@ -14,9 +13,8 @@ const store = new Vuex.Store({
     modules: {
         auth,
         error,
-        createForm,
+        problem,
         listProblem,
-        playProblem,
         flashMessage
     }
 });
