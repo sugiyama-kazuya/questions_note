5534cc04d5116a337c37ac5be48257af4ee4c362
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-06-14 10:42:45 +0900

    問題作成画面のフォームをコンポーネントとして切り分け

---
+++ resources/js/components/form/FormLabel.vue
@@ -0,0 +1,14 @@
+<template>
+  <label class="px-3 block text-gray-700 m-0 font-bold" for="problem">
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "FormLabel"
+};
+</script>
+
+<style>
+</style>
---
+++ resources/js/components/form/FormValidationErrorMessage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p
+      v-for="message in errorMessages"
+      :key="message"
+      class="text-red-500 text-xs italic"
+    >{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormValidationErrorMessage",
+  props: {
+    errorMessages: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
