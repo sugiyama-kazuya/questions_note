17d008992fca7fe31cab4825ca2f08bc50e8cdad
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-06-16 20:39:34 +0900

    カテゴリーテーブルの作成、FKの作成

--- database/migrations/2020_06_14_065800_add_user_id_column_categories_table.php
+++ database/migrations/2020_06_14_065800_add_user_id_column_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateExerciseBooksTable extends Migration
+class AddUserIdColumnCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateExerciseBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('exercise_books', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name')->comment('問題集の名前');
+        Schema::table('categories', function (Blueprint $table) {
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->bigInteger('problem_id')->unsigned();
-            $table->foreign('problem_id')->references('id')->on('problems');
-            $table->timestamps();
         });
     }
 
@@ -31,6 +26,9 @@ class CreateExerciseBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('exercise_books');
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeign('categories_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
     }
 }
