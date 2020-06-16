fb1e291a97e9aa576e7fcd0aa88b31137970ef87
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-06-16 20:33:44 +0900

    カテゴリーの作成時に必要なアクセサを記述、関数化

---
+++ app/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name', 'user_id'
+    ];
+
+    /**
+     * ログインユーザーのカテゴリーを取得
+     *
+     * @return Object
+     */
+    public function getLoginUserCategoryAttribute()
+    {
+        return $this->where('user_id', Auth::id())->get(['id', 'name']);
+    }
+
+    /**
+     * カテゴリーの登録
+     * 対象のユーザーのカテゴリーデータを取得し、既に登録されているなら取得、されていないなら登録
+     *
+     * @param [object] $request
+     * @return object
+     */
+    public function fetchOrRegister($request)
+    {
+        return $this->where('user_id', Auth::id())
+            ->firstOrCreate(
+                ['name' => $request->category],
+                [
+                    'name' => $request->category,
+                    'user_id' => Auth::id()
+                ]
+            );
+    }
+}
