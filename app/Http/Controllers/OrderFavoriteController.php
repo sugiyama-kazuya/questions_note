85bafe8d0391e42d0a179a888c7e41f2d0cb799c
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-06-20 13:22:55 +0900

    デバッグ処理の削除

--- app/Http/Controllers/OrderFavoriteController.php
+++ app/Http/Controllers/OrderFavoriteController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\ExerciseBook;
-use Illuminate\Support\Facades\Log;
 
 class OrderFavoriteController extends Controller
 {
@@ -12,12 +11,11 @@ class OrderFavoriteController extends Controller
         $exercise_books = $exercise_book->exerciseBookCardRequiredData()->get();
         $exercise_books = $exercise_book->addProfileUrl($exercise_books);
         $exercise_books = $exercise_book->addFavoriteInfo($exercise_books);
+
         $exercise_books = $exercise_book->filteringRequiredData($exercise_books);
         $exercise_books = $exercise_books->sortByDesc(function ($data) {
             return $data['favolite_count'];
         })->values();
-
-        Log::debug($exercise_books);
 
 
         return response()->json(['exercise_books' => $exercise_books]);
