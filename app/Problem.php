b829433ffbd7c8b80b31315620dd6f85ecb9667e
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-06-22 11:15:14 +0900

    jsonに含めないプロパティの設定と問題集テーブルへのリレーションの設定

--- app/Problem.php
+++ app/Problem.php
@@ -3,10 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Problem extends Model
 {
     protected $fillable = [
         'content', 'answer', 'user_id', 'exercise_book_id', 'url'
+    ];
+
+    public function exerciseBook(): BelongsTo
+    {
+        return $this->belongsTo('App\ExerciseBook');
+    }
+
+    protected $hidden = [
+        'created_at', 'user_id', 'exercise_book_id'
     ];
 }
