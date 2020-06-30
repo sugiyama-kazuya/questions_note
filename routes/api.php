b9c547bf9d225034d2f0c193c406a8418f24e337
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-06-30 17:12:10 +0900

    エンドポイント設計の見直しのため、URIの変更

--- routes/api.php
+++ routes/api.php
@@ -3,39 +3,38 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
 Route::post('/login', 'Auth\LoginController@login')->name('login');
+
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/user', function () {
-    return Auth::user();
-})->name('user');
+Route::resource('problems', 'ProblemController')->except(['index']);
 
-Route::resource('problems', 'ProblemController');
+Route::resource('exercise-books', 'ExerciseBookController')->only(['index', 'store', 'show']);
+Route::prefix('exercise-books')->name('exercise-books.')->group(function () {
+    Route::put('favorites', 'Favorites')->name('like');
+    Route::delete('unfavorites', 'unFavorites')->name('unlike');
+    Route::get('favorites/asc', 'FavoritesOrderByAsc');
+    Route::get('favorites/asc/{user_id}', 'OwnFavoritesOrderByAsc');
+    Route::get('{id}/favorites/counts', 'FavoritesCount');
+});
 
-Route::resource('exercise-books', 'ExerciseBookController')->only(['store', 'show', 'index']);
-// Route::post('exercise-books', 'CreateExerciseBookCheck');
-// Route::get('ownExercizeBooks/{id}', 'GetOwnExercizeBooksController');
+Route::resource('users', 'UserController')->only(['show', 'edit', 'update']);
+Route::prefix('users')->name('users.')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::put('{id}/follow', 'Follow')->name('follow');
+        Route::delete('{id}/follow', 'unFollow')->name('unfollow');
+        Route::get('{id}/followers', 'FollowersCount')->name('followers');
+        Route::get('{id}/follwings', 'FollwingsCount')->name('follwings');
+    });
+});
 
 Route::resource('categories', 'CategoryController')->only(['store']);
 
-// Route::post('problems/categoryCheck', 'CategoryDuplicateCheck');
-Route::resource('profile', 'ProfileController')->only(['show', 'edit', 'update']);
 
-// 問題集の取得（項目別）
-Route::get('order-favorite-exercise-books', 'OrderFavoriteController');
-Route::get('own-favorite-exercise-books', 'OwnFavoriteExerciseBooksController');
+
+
 
 
 // お気に入り関連
@@ -43,15 +42,9 @@ Route::put('like', 'LikeController@like')->name('like');
 Route::delete('unlike', 'LikeController@unlike')->name('unlike');
 
 Route::get('isLikedby/{id}', 'isLikedByController');
-Route::get('likes/{id}/count', 'CountLikesController');
+Route::get('{id}/favorites/counts', 'FavoritesCount');
 
 
-// user関連
-Route::prefix('user')->name('user.')->group(function () {
-    Route::middleware('auth')->group(function () {
-        Route::put('{id}/follow', 'UserController@follow')->name('follow');
-        Route::delete('{id}/follow', 'UserController@unfollow')->name('unfollow');
-        Route::get('{id}/followers', 'UserController@followersCount')->name('followers');
-        Route::get('{id}/follwings', 'UserController@follwingsCount')->name('follwings');
-    });
-});
+Route::get('/user', function () {
+    return Auth::user();
+})->name('user');
