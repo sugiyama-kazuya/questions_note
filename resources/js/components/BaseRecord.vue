dcad7b36ca1e1c33f580b57374e53f0c5a4eba00
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-01 17:43:52 +0900

    管理画面などで使用されているレコード部分をコンポーネント化

---
+++ resources/js/components/BaseRecord.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-center items-center w-100 mb-3">
+    <div
+      class="flex justify-between items-center w-4/5 rounded-md shadow-md border"
+      :class="padding ? padding : ''"
+    >
+      <div class="flex items-center">
+        <slot name="left-contents"></slot>
+      </div>
+      <slot name="right-contents"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseRecord",
+  props: {
+    padding: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+</style>
