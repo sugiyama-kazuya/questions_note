76ebd66295f43edd9a0606c8f6bc3a672ef7d359
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-04 21:02:40 +0900

    問題集削除の際に紐づいてる問題を一括削除する為に、オブサーバを作成、プロバイダーに登録

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\ExerciseBook;
+use App\Observers\ExerciseBookObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ExerciseBook::observe(ExerciseBookObserver::class);
     }
 }
