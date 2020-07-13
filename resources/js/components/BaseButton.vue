067662f519578723af9cae6456b4bef93a2cee8c
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-13 18:16:41 +0900

    ボタンのボーダーの色を追加

--- resources/js/components/BaseButton.vue
+++ resources/js/components/BaseButton.vue
@@ -1,41 +1,48 @@
 <template>
-  <button
-    @click="$emit('click-btn')"
-    :class="[
+    <button
+        @click="$emit('click-btn')"
+        :class="[
             height ? height : '',
             width ? width : '',
             color ? color : 'bg-blue-400',
-            text ? text : 'text-white'
+            text ? text : 'text-white',
+            borderColor ? 'border-blue' : ''
         ]"
-    :type="type ? type : 'button'"
-    class="p-2 rounded-sm shadow-sm font-bold focus:outline-none outline-none"
-  >
-    <slot></slot>
-  </button>
+        :type="type ? type : 'button'"
+        class="p-2 rounded-sm shadow-sm font-bold focus:outline-none outline-none"
+    >
+        <slot></slot>
+    </button>
 </template>
 
 <script>
 export default {
-  name: "BaseButton",
-  props: {
-    height: {
-      type: String
-    },
-    width: {
-      type: String
-    },
-    color: {
-      type: String
-    },
-    text: {
-      type: String
-    },
-    type: {
-      type: String
+    name: "BaseButton",
+    props: {
+        height: {
+            type: String
+        },
+        width: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        text: {
+            type: String
+        },
+        type: {
+            type: String
+        },
+        borderColor: {
+            type: Boolean
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
+.border-blue {
+    border: 1px solid #63b3ed;
+}
 </style>
