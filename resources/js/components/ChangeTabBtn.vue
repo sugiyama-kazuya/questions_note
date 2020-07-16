4c7830c002b3a44eaad2c244455dc1a646ab6a9d
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-16 17:05:43 +0900

    List画面に変更になった為、遷移先の指定などの変更

--- resources/js/components/ChangeTabBtn.vue
+++ resources/js/components/ChangeTabBtn.vue
@@ -1,44 +1,44 @@
 <template>
-  <div class="inline-flex st-border" :class="width ? width : 'w-4/5'">
-    <button
-      @click="$emit('left-click')"
-      class="w-1/2 bg-white text-indigo-500 py-2 px-4 focus:outline-none"
-      :class="{ 'is-tab-active': isLeftActive }"
-    >
-      <slot name="leftBtnText" />
-    </button>
-    <button
-      @click="$emit('right-click')"
-      class="w-1/2 bg-white py-2 px-4 text-indigo-500 focus:outline-none"
-      :class="{ 'is-tab-active': isRightActive }"
-    >
-      <slot name="rightBtnText" />
-    </button>
-  </div>
+    <div class="inline-flex st-border" :class="width ? width : 'w-4/5'">
+        <button
+            @click="$emit('left-click')"
+            class="w-1/2 bg-white text-indigo-500 py-2 px-4 focus:outline-none"
+            :class="{ 'is-tab-active': isLeftActive }"
+        >
+            <slot name="leftBtnText" />
+        </button>
+        <button
+            @click="$emit('right-click')"
+            class="w-1/2 bg-white py-2 px-4 text-indigo-500 focus:outline-none"
+            :class="{ 'is-tab-active': isRightActive }"
+        >
+            <slot name="rightBtnText" />
+        </button>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    isLeftActive: {
-      type: Boolean
-    },
-    isRightActive: {
-      type: Boolean
-    },
-    width: {
-      type: String
+    props: {
+        isLeftActive: {
+            type: Boolean
+        },
+        isRightActive: {
+            type: Boolean
+        },
+        width: {
+            type: String
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
 .is-tab-active {
-  background-color: #667eea !important;
-  color: #fff !important;
+    background-color: #667eea !important;
+    color: #fff !important;
 }
 .st-border {
-  border: 1px solid #667eea;
+    border: 1px solid #667eea;
 }
 </style>
--- resources/js/components/TheFooter.vue
+++ resources/js/components/TheFooter.vue
@@ -1,72 +1,74 @@
 <template>
-  <footer class="flex py-3 bg-indigo-400 absolute inset-x-0 bottom-0 h-10 text-white">
-    <div class="w-1/4 flex flex-col items-center justify-center">
-      <router-link
-        to="/exercise-books"
-        tag="div"
-        class="flex flex-col items-center justify-center"
-        active-class="text-yellow-300"
-        exact
-      >
-        <font-awesome-icon icon="book-open" class="text-3xl" />Index
-      </router-link>
-    </div>
-    <div class="w-1/4 flex flex-col items-center justify-center">
-      <router-link
-        to="/problems/create"
-        tag="div"
-        class="flex flex-col items-center justify-center"
-        active-class="text-yellow-300"
-        exact
-      >
-        <font-awesome-icon icon="plus" class="text-3xl" />Create
-      </router-link>
-    </div>
-    <div class="w-1/4 flex flex-col items-center justify-center">
-      <router-link
-        :to="{ name: 'profile', params: { id: loginUserId } }"
-        tag="div"
-        class="flex flex-col items-center justify-center"
-        active-class="text-yellow-300"
-        exact
-      >
-        <font-awesome-icon icon="home" class="text-3xl" />Home
-      </router-link>
-    </div>
-    <div class="w-1/4 flex flex-col items-center justify-center">
-      <router-link
-        to="/search"
-        tag="div"
-        class="flex flex-col items-center justify-center"
-        active-class="text-yellow-300"
-        exact
-      >
-        <font-awesome-icon icon="search" class="text-3xl" />Search
-      </router-link>
-    </div>
-  </footer>
+    <footer
+        class="flex py-3 bg-indigo-400 absolute inset-x-0 bottom-0 h-10 text-white"
+    >
+        <div class="w-1/4 flex flex-col items-center justify-center">
+            <router-link
+                to="/exercise-books"
+                tag="div"
+                class="flex flex-col items-center justify-center"
+                active-class="text-yellow-300"
+                exact
+            >
+                <font-awesome-icon icon="book-open" class="text-3xl" />Index
+            </router-link>
+        </div>
+        <div class="w-1/4 flex flex-col items-center justify-center">
+            <router-link
+                to="/problems/create"
+                tag="div"
+                class="flex flex-col items-center justify-center"
+                active-class="text-yellow-300"
+                exact
+            >
+                <font-awesome-icon icon="plus" class="text-3xl" />Create
+            </router-link>
+        </div>
+        <div class="w-1/4 flex flex-col items-center justify-center">
+            <router-link
+                :to="{ name: 'profile', params: { id: loginUserId } }"
+                tag="div"
+                class="flex flex-col items-center justify-center"
+                active-class="text-yellow-300"
+                exact
+            >
+                <font-awesome-icon icon="home" class="text-3xl" />Home
+            </router-link>
+        </div>
+        <div class="w-1/4 flex flex-col items-center justify-center">
+            <router-link
+                to="/list"
+                tag="div"
+                class="flex flex-col items-center justify-center"
+                active-class="text-yellow-300"
+                exact
+            >
+                <font-awesome-icon icon="list-ul" class="text-3xl" />List
+            </router-link>
+        </div>
+    </footer>
 </template>
 
 <script>
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-  faHome,
-  faPlus,
-  faBookOpen,
-  faSearch
+    faHome,
+    faPlus,
+    faBookOpen,
+    faListUl
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-library.add(faHome, faPlus, faBookOpen, faSearch);
+library.add(faHome, faPlus, faBookOpen, faListUl);
 
 export default {
-  name: "TheFooter",
-  components: {
-    FontAwesomeIcon
-  },
-  computed: {
-    loginUserId() {
-      return this.$store.state.auth.user.id;
+    name: "TheFooter",
+    components: {
+        FontAwesomeIcon
+    },
+    computed: {
+        loginUserId() {
+            return this.$store.state.auth.user.id;
+        }
     }
-  }
 };
 </script>
