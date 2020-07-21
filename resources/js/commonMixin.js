a1aaa8e339f10149c39b21c76030be946cacceab
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-21 16:28:04 +0900

    スクロール量をトップに戻す関数をミックスインとした作成

---
+++ resources/js/commonMixin.js
@@ -0,0 +1,10 @@
+export default {
+    methods: {
+        scrollTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: "instant"
+            });
+        }
+    }
+};
