a1968e714e2721c86b80c9a66f92f8b7a3fce99d
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-21 16:33:20 +0900

    親コンポーネントから発生させるイベントリスナーを作成、テキストエリア内の背景色を変更できるようにpropsを作成。

--- resources/js/components/BaseSearchBox.vue
+++ resources/js/components/BaseSearchBox.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="flex justify-center w-screen mb-4">
-    <form class="flex w-4/5 rounded-full">
-      <input
-        type="text"
-        class="w-5/6 py-2 px-3 bg-gray-200 rounded-l-full focus:outline-none"
-        :placeholder="placeholder ? placeholder : ''"
-      />
-      <button class="w-1/6 bg-blue-300 rounded-r-full">
-        <FontAwesomeIcon icon="search" class="text-1xl" />
-      </button>
-    </form>
-  </div>
+    <div class="flex justify-center w-screen text-gray-600">
+        <form class="flex w-4/5 rounded-full">
+            <input
+                :value="value"
+                @input="$emit('input', $event.target.value)"
+                type="text"
+                class="w-5/6 py-2 px-3
+            rounded-l-full focus:outline-none"
+                :class="bgColor ? bgColor : 'bg-gray-200'"
+                :placeholder="placeholder ? placeholder : ''"
+            />
+            <button
+                @click.prevent="$emit('search')"
+                class="w-1/6 bg-blue-300 rounded-r-full focus:outline-none outline-none"
+            >
+                <FontAwesomeIcon icon="search" class="text-1xl" />
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -20,21 +27,21 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 library.add(faSearch);
 
 export default {
-  name: "BaseSearchBox",
-  components: {
-    FontAwesomeIcon
-  },
-  props: {
-    placeholder: {
-      type: String,
-      required: true
+    name: "BaseSearchBox",
+    components: {
+        FontAwesomeIcon
+    },
+    props: {
+        value: {
+            type: String
+        },
+        placeholder: {
+            type: String,
+            required: true
+        },
+        bgColor: {
+            type: String
+        }
     }
-  },
-  data() {
-    return {};
-  }
 };
 </script>
-
-<style>
-</style>
