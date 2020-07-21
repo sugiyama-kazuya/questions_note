0eb9d8930988593eb495a2c21e959859ea81c900
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-21 16:26:54 +0900

    問題の新規登録のロジックをモデルに切り分ける

--- app/Problem.php
+++ app/Problem.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Problem extends Model
 {
@@ -19,4 +20,22 @@ class Problem extends Model
     protected $hidden = [
         'created_at', 'user_id', 'exercise_book_id'
     ];
+
+    /**
+     * 問題と答えの新規登録
+     *
+     * @param [object]] $request
+     * @param [object] $exercise_book
+     * @return void
+     */
+    public function newRegister($request, $exercise_book)
+    {
+        $this->fill([
+            'content' => $request->problem,
+            'answer' => $request->answer,
+            'url' => $request->url,
+            'user_id' => Auth::id(),
+            'exercise_book_id' => $exercise_book->id,
+        ])->save();
+    }
 }
