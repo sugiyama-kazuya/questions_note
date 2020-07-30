3273b97d5fc734cc80dd23864705edc17e8a0a33
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-30 14:23:55 +0900

    画像の削除時に使用しているボタンをコンポーネント化

---
+++ resources/js/components/CancelButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <span @click="$emit('click-btn')" class="absolute top-0 right-0 mr-2 mt-2">
+        <font-awesome-icon icon="times-circle" class="text-4xl text-blue-500" />
+    </span>
+</template>
+
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faTimesCircle);
+
+export default {
+    name: "CancelButton",
+    components: {
+        FontAwesomeIcon
+    }
+};
+</script>
+
+<style></style>
