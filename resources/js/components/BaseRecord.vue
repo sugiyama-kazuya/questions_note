af8a64bb818f66ba5b18442a62ec6abaa3ba2673
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-07-30 15:38:28 +0900

    レコードコンポーネントにprorpsを追加

--- resources/js/components/BaseRecord.vue
+++ resources/js/components/BaseRecord.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="flex justify-center items-center w-100 mb-3">
         <div
-            class="flex items-center w-4/5 rounded-md shadow-md border bg-white"
-            :class="padding ? padding : ''"
+            class="flex items-center w-4/5 rounded-md shadow-md border"
+            :class="[
+                padding ? padding : '',
+                backgroundColor ? backgroundColor : 'bg-white'
+            ]"
         >
             <div class="flex justify-start items-center w-1/2">
                 <slot name="left-contents"></slot>
@@ -18,6 +21,9 @@
 export default {
     name: "BaseRecord",
     props: {
+        backgroundColor: {
+            type: String
+        },
         padding: {
             type: String
         }
