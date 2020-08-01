e2bfc1602c516e2586229b352855200f00ee7509
Kazuya Sugiyama <kakao.0451a@gmail.com>
2020-08-01 13:34:46 +0900

    問題集のダミーデータの作成

---
+++ database/seeds/ExerciseBooksTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\ExerciseBook;
+use Illuminate\Database\Seeder;
+
+class ExerciseBooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(ExerciseBook::class, 150)->create();
+    }
+}
